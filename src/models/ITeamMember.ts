717f66581be955e337bb209888a8efc0e0cf4029
Tom Schmiedlechner <tom.schmiedlechner@fiskaltrust.services>
2021-06-04 15:45:39 +0200

    Cleanup

--- src/models/ITeamMember.ts
+++
@@ -1,6 +0,0 @@
-export interface ITeamMember {
-    name: string,
-    role: string,
-    email?: string,
-    imageUrl?: string
-}
--- src/models/ITeamPosition.ts
+++
@@ -1,4 +0,0 @@
-export interface ITeamPosition {
-    x: number,
-    y: number
-}
